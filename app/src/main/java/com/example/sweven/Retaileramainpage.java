38850879fb55e95994d972edc7c359985bbb7b1f
AakankshaKamal <aakankshakamal.post@gmail.com>
2020-12-12 12:14:37 +0530

    signin all three buttons added only databse need to be created

---
+++ app/src/main/java/com/example/sweven/Retaileramainpage.java
@@ -0,0 +1,13 @@
+package com.example.sweven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Retaileramainpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_retaileramainpage);
+    }
+}
---
+++ app/src/main/java/com/example/sweven/warehousemain.java
@@ -0,0 +1,13 @@
+package com.example.sweven;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class warehousemain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_warehousemain);
+    }
+}
